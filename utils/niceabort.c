9f88450243e9a76ed12a58345b60a9ebbb48a1bd
Darryl L. Miles <darryl.miles@darrylmiles.org>
2024-10-02 06:59:00 +0100

    niceabort.c: '%s' directive writing up to 199 bytes into a region of size 177
    GCC14 loud default warnings series [-Wformat-overflow=]

--- utils/niceabort.c
+++ utils/niceabort.c
@@ -67,7 +67,7 @@ DumpCore()
     int cpid, gcpid, wpid;
     FILE *commentFile, *crashFile;
     time_t now;
-    char pidString[20], line[150], command[200], tempName[200], *crashDir;
+    char pidString[20], line[150], command[512], tempName[200], *crashDir;
 
     TxPrintf("Please wait while magic generates a core image of itself....\n");
     sprintf(pidString, "%d", parentPid);
