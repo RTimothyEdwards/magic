b6775f902be79c56bd8b6e10f97d3a0ebf6a2a0f
Darryl L. Miles <darryl.miles@darrylmiles.org>
2024-10-04 11:37:02 +0100

    cif: use 'const' with 'rcsid'

--- cif/CIFrdpt.c
+++ cif/CIFrdpt.c
@@ -18,7 +18,7 @@
  */
 
 #ifndef lint
-static char rcsid[] __attribute__ ((unused)) = "$Header: /usr/cvsroot/magic-8.0/cif/CIFrdpt.c,v 1.2 2010/06/24 12:37:15 tim Exp $";
+static const char rcsid[] __attribute__ ((unused)) = "$Header: /usr/cvsroot/magic-8.0/cif/CIFrdpt.c,v 1.2 2010/06/24 12:37:15 tim Exp $";
 #endif  /* not lint */
 
 #include <stdio.h>
