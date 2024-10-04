4f81e0d166469328fab66c77293157d8b29e2745
Darryl L. Miles <darryl.miles@darrylmiles.org>
2024-10-04 11:19:10 +0100

    CalmaRdpt.c: simplfy use of sprintf()
    This replacement pattern is easier to reason about and less error prone.

--- calma/CalmaRdpt.c
+++ calma/CalmaRdpt.c
@@ -257,13 +257,13 @@ calmaElementBoundary(void)
     if ((CalmaSubcellPolygons != CALMA_POLYGON_NONE) && (calmaNonManhattan > 0))
     {
 	/* Place the polygon in its own subcell */
-	char newname[] = "polygonXXXXX";
+	char newname[16];
 	HashEntry *he;
 
 	savedef = cifReadCellDef;
 
 	/* Make up name for cell */
-	sprintf(newname + 7, "%05d", ++CalmaPolygonCount);
+	snprintf(newname, sizeof(newname), "polygon%05d", ++CalmaPolygonCount);
 
 	he = HashFind(&calmaDefInitHash, newname);
 	if (!HashGetValue(he))
@@ -663,13 +663,13 @@ calmaElementPath(void)
 	if (CalmaSubcellPaths)
 	{
 	    /* Place the path in its own subcell */
-	    char newname[] = "pathXXXXX";
+	    char newname[16];
 	    HashEntry *he;
 
 	    savedef = cifReadCellDef;
 
 	    /* Make up name for cell */
-	    sprintf(newname + 4, "%05d", ++CalmaPathCount);
+	    snprintf(newname, sizeof(newname), "path%05d", ++CalmaPathCount);
 
 	    he = HashFind(&calmaDefInitHash, newname);
 	    if (!HashGetValue(he))
