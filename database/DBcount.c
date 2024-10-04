9d235117202900563171fa909e586576c579051a
Darryl L. Miles <darryl.miles@darrylmiles.org>
2024-10-04 12:17:24 +0100

    DBcount.c: DBTreeCountPaint() unify return type of hiercount() callback
    The call-site does not expect a return value.
    The use case in command/Cmdwizard.c is written to be void.
    
    So a resolution was needed.

--- database/DBcount.c
+++ database/DBcount.c
@@ -50,7 +50,7 @@ static char rcsid[] __attribute__ ((unused)) = "$Header: /usr/cvsroot/magic-8.0/
  * cell graph; if it returns 0 then the subcells of 'def' are visited;
  * if it returns 1 then the subcells are not visited.
  *
- *	int
+ *	void
  *	hiercount(parent, uses, child, cdata)
  *	    CellDef *parent;
  *	    int uses;		/# Scale factor: number of times child
@@ -104,7 +104,7 @@ static char rcsid[] __attribute__ ((unused)) = "$Header: /usr/cvsroot/magic-8.0/
 struct countArg
 {
     int (*ca_count)();
-    int (*ca_hiercount)();
+    void (*ca_hiercount)();
     ClientData ca_cdata;
 };
 
@@ -112,7 +112,7 @@ void
 DBTreeCountPaint(def, count, hiercount, cleanup, cdata)
     CellDef *def;
     int (*count)();
-    int (*hiercount)();
+    void (*hiercount)();
     int (*cleanup)();
     ClientData cdata;
 {
