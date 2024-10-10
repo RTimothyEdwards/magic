6851f272847eb7626cac4dd30dfcb51a29e8d68d
Darryl L. Miles <darryl.miles@darrylmiles.org>
2024-10-10 20:20:26 +0100

    extract/ExtBasic.c: Lookup() constify call-site

--- extflat/EFread.c
+++ extflat/EFread.c
@@ -47,7 +47,7 @@ static char rcsid[] __attribute__ ((unused)) = "$Header: /usr/cvsroot/magic-8.0/
 
 #ifndef MAGIC_WRAPPER
 /* This must match the definition for extDevTable in extract/ExtBasic.c */
-char *extDevTable[] = {"fet", "mosfet", "asymmetric", "bjt", "devres",
+const char * const extDevTable[] = {"fet", "mosfet", "asymmetric", "bjt", "devres",
 		"devcap", "devcaprev", "vsource", "diode", "pdiode",
 		"ndiode", "subckt", "rsubckt", "msubckt", "csubckt", NULL};
 #endif
