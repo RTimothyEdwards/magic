a605b641694ff24ca8320b22bf6ace8c941ae95b
Darryl L. Miles <darryl.miles@darrylmiles.org>
2025-01-31 19:39:09 +0000

    K&R sim.h: conversion to ANSI
    K&R obsolete syntax removal for C23 compatibility series

--- sim/sim.h
+++ sim/sim.h
@@ -2,16 +2,18 @@
 #define _SIM_H
 
 #include "utils/magic.h"
+#include "textio/txcommands.h"	/* TxCommand */
+#include "windows/windows.h"	/* MagWindow */
 
-extern	char	*SimGetNodeCommand();
-extern	char	*SimGetNodeName();
-extern	char	*SimSelectNode();
-extern	bool	SimGetReplyLine();
-extern	void	SimRsimIt();
-extern	void	SimEraseLabels();
-extern	bool	efPreferredName();
-extern  void	SimRsimHandler();
-extern  void	SimInit();
+extern	char	*SimGetNodeCommand(char *cmd);
+extern	char	*SimGetNodeName(SearchContext *sx, Tile *tp, char *path);
+extern	char	*SimSelectNode(SearchContext *scx, TileType type, int xMask, char *buffer);
+extern	bool	SimGetReplyLine(char **replyLine);
+extern	void	SimRsimIt(char *cmd, char *nodeName);
+extern	void	SimEraseLabels(void);
+extern	bool	efPreferredName(char *name1, char *name2);
+extern  void	SimRsimHandler(MagWindow *w, TxCommand *cmd);
+extern  void	SimInit(void);
 
 extern  bool	SimRecomputeSel;
 extern 	bool	SimInitGetnode;
@@ -29,18 +31,22 @@ extern HashTable SimGetnodeTbl;
 extern HashTable SimAbortSeenTbl;
 
 /* C99 compat */
-extern void SimGetnode();
-extern void SimGetsnode();
-extern void SimGetNodeCleanUp();
-extern int  SimPutLabel();
-extern int  SimSrConnect();
-extern void SimTreeCopyConnect();
-extern int  SimTreeSrNMTiles();
-extern int  SimTreeSrTiles();
-extern bool SimStartRsim();
-extern void SimConnectRsim();
-extern bool SimSelection();
-extern void SimRsimMouse();
-extern int  SimFillBuffer();
+extern void SimGetnode(void);
+extern void SimGetsnode(void);
+extern void SimGetNodeCleanUp(void);
+extern int SimPutLabel(CellDef *cellDef, Rect *rect, int align, char *text, TileType type);
+extern int SimSrConnect(CellDef *def, Rect *startArea, TileTypeBitMask *mask, TileTypeBitMask *connect,
+                        Rect *bounds, int (*func)(), ClientData clientData);
+extern void SimTreeCopyConnect(SearchContext *scx, TileTypeBitMask *mask, int xMask, TileTypeBitMask *connect,
+                               Rect *area, CellUse *destUse, char *Node_Name);
+extern int SimTreeSrNMTiles(SearchContext *scx, TileType dinfo, TileTypeBitMask *mask, int xMask, TerminalPath *tpath,
+                            int (*func)(), ClientData cdarg);
+extern int SimTreeSrTiles(SearchContext *scx, TileTypeBitMask *mask, int xMask, TerminalPath *tpath,
+                          int (*func)(), ClientData cdarg);
+extern bool SimStartRsim(char *argv[]);
+extern void SimConnectRsim(bool escRsim);
+extern bool SimSelection(char *cmd);
+extern void SimRsimMouse(MagWindow *w);
+extern int SimFillBuffer(char *buffHead, char **pLastChar, int *charCount);
 
 #endif /* _SIM_H */
