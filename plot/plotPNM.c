RTimothyEdwards/magic

commit e88dcba1c5777b31eae5220ffcbcfc4fb9a219dd

CodeQL File{MayNot,Never}BeClosed.ql file-handle resource leaks
Guided by CodeQL static code analyser.

FileMayNotBeClosed.ql
FileMayNeverBeClosed.ql

The trick with "if(fp != stdout)" is problematic (to analyser) as
technically 'stdout' can be a global pointer that COULD be modified any
time, so it might have changed between the fopen() and fclose() calls so
the close MAY NEVER occurs (which is problem the analyzer can see).

So local state is maintained as a bool which will also clarify to the
compiler see the intention without concern for external stdout
modification.

Some items appear to be out and out leaks when certain commands are use.

plot/plotPNM.c
@@ -576,7 +576,7 @@ PlotPNM(fileName, scx, layers, xMask, width)
 					 * plot, in pixels.
 					 */
 {
-    FILE *fp;
+    FILE *fp = NULL;
     Rect bbox;
     int bb_ysize, bb_xsize;
     int i, x, y, tile_ydelta;
@@ -920,6 +920,7 @@ PlotPNM(fileName, scx, layers, xMask, width)
 	}
 	fflush(rtl_args.outfile);
 	fclose(rtl_args.outfile);
+	rtl_args.outfile = NULL;
 	freeMagic(rtl_args.outbytes);
 
 	/* Run spooler */
@@ -932,7 +933,13 @@ PlotPNM(fileName, scx, layers, xMask, width)
     }
     else
 #endif
-	fclose (fp);
+    {
+        if(fp)
+        {
+	    fclose (fp);
+	    fp = NULL;
+        }
+    }
 
 done:
     PlotPNMdownsample = save_ds;
@@ -941,6 +948,15 @@ done:
     freeMagic(strip);
     freeMagic(lkstep);
     lkstep = NULL;
+#ifdef VERSATEC
+    if(rtl_args.outfile) /* theoretical fp leak */
+    {
+	fclose(rtl_args.outfile);
+	rtl_args.outfile = NULL;
+    }
+#endif
+    if(fp)
+	fclose(fp);
     return;
 }
 

commands/CmdCD.c
@@ -3994,7 +3994,6 @@ CmdDrc(
     MagWindow *w,
     TxCommand *cmd)
 {
-    FILE        * fp;
     static int	drc_nth = 1;
     int		  option, result, radius;
     Rect	  rootArea, area;
@@ -4475,15 +4474,19 @@ CmdDrc(
 	case PRINTRULES:
 	    if (argc > 3) goto badusage;
 	    if (argc < 3)
-		fp = stdout;
-	    else if ((fp = fopen (argv[2],"w")) == (FILE *) NULL)
 	    {
-		TxError("Cannot write file %s\n", argv[2]);
-		return;
+		DRCPrintRulesTable (stdout);
 	    }
-	    DRCPrintRulesTable (fp);
-	    if (fp != stdout)
-		(void) fclose(fp);
+	    {
+	        FILE *fp = fopen (argv[2], "w");
+	        if (fp == NULL)
+		{
+		    TxError("Cannot write file %s\n", argv[2]);
+		    return;
+		}
+		DRCPrintRulesTable (fp);
+		fclose(fp);
+	    }
 	    break;
 
 	case RULESTATS:

commands/CmdTZ.c
@@ -1821,6 +1821,7 @@ CmdWire(
 				TxError("Bad coordinate pair at %s line %d\n",
 					cmd->tx_argv[4], i + 1);
 				freeMagic(plist);
+				fclose(pfile);
 				return;
 			    }
 

extract/ExtTest.c
@@ -119,7 +119,6 @@ ExtractTest(w, cmd)
     CellUse *selectedCell;
     Rect editArea;
     char *addr, *name;
-    FILE *f;
     typedef enum {  CLRDEBUG, CLRLENGTH, DRIVER, DUMP, INTERACTIONS,
 		    INTERCOUNT, EXTPARENTS, RECEIVER, SETDEBUG, SHOWDEBUG,
 		    SHOWPARENTS, SHOWTECH, STATS, STEP, TIME } cmdType;
@@ -215,37 +214,41 @@ ExtractTest(w, cmd)
 	    DBClearPaintPlane(interPlane);
 	    break;
 	case INTERCOUNT:
-	    f = stdout;
 	    halo = 1;
 	    if (cmd->tx_argc > 2)
 		halo = atoi(cmd->tx_argv[2]);
 	    if (cmd->tx_argc > 3)
 	    {
-		f = fopen(cmd->tx_argv[3], "w");
+		FILE *f = fopen(cmd->tx_argv[3], "w");
 		if (f == NULL)
 		{
 		    perror(cmd->tx_argv[3]);
 		    break;
 		}
+		ExtInterCount((CellUse *) w->w_surfaceID, halo, f);
+		fclose(f);
 	    }
-	    ExtInterCount((CellUse *) w->w_surfaceID, halo, f);
-	    if (f != stdout)
-		(void) fclose(f);
+	    else
+	    {
+		ExtInterCount((CellUse *) w->w_surfaceID, halo, stdout);
+	    }
 	    break;
 	case TIME:
-	    f = stdout;
 	    if (cmd->tx_argc > 2)
 	    {
-		f = fopen(cmd->tx_argv[2], "w");
+		FILE *f = fopen(cmd->tx_argv[2], "w");
 		if (f == NULL)
 		{
 		    perror(cmd->tx_argv[2]);
 		    break;
 		}
+		ExtTimes((CellUse *) w->w_surfaceID, f);
+		fclose(f);
 	    }
-	    ExtTimes((CellUse *) w->w_surfaceID, f);
-	    if (f != stdout)
-		(void) fclose(f);
+	    else
+	    {
+		ExtTimes((CellUse *) w->w_surfaceID, stdout);
+	    }
 	    break;
 	case EXTPARENTS:
 	    if (ToolGetEditBox(&editArea))
@@ -1105,6 +1108,7 @@ ExtDumpCaps(filename)
 	    return;
 	}
 	ExtDumpCapsToFile(f);
+	fclose(f);
 	return;
     }
 

graphics/grCMap.c
@@ -122,7 +122,6 @@ char *path;			/* a search path */
 char *libPath;			/* a library search path */
 
 {
-    FILE *f;
     int max, red, green, blue, newmax, argc, i;
     colorEntry *ce;
     char fullName[256], inputLine[128], colorName[100];
@@ -135,7 +134,7 @@ char *libPath;			/* a library search path */
     (void) sprintf(fullName, "%.80s.%.80s.%.80s", techStyle,
 	    dispType, monType);
 
-    f = PaOpen(fullName, "r", ".cmap", path, libPath, (char **) NULL);
+    FILE *f = PaOpen(fullName, "r", ".cmap", path, libPath, (char **) NULL);
     if (f == NULL)
     {
 	/* Check for original ".cmap1" file (prior to magic v. 7.2.27) */
@@ -163,7 +162,7 @@ char *libPath;			/* a library search path */
 
 	    TxError("Syntax error in color map file \"%s.cmap\"\n", fullName);
 	    TxError("Last color read was index %d\n", max);
-	    return FALSE;
+	    goto cleanup;
 	}
 	else
 	{
@@ -219,6 +218,11 @@ char *libPath;			/* a library search path */
 
     GrSetCMap();
     return TRUE;
+
+cleanup:
+    if(f)
+	fclose(f);
+    return FALSE;
 }
 
 

plot/plotPS.c
@@ -1135,7 +1135,6 @@ PlotPS(fileName, scx, layers, xMask)
 {
     int xsize, ysize;
     float yscale;
-    FILE *infile;
     int i, j;
     int twidth, theight;
     char *fontptr, *fptr2, *fptr3;
@@ -1207,12 +1206,17 @@ PlotPS(fileName, scx, layers, xMask)
 
     /* Insert the prolog here */
 
-    infile = PaOpen("magicps", "r", ".pro", ".", SysLibPath, NULL);
-    if (infile != NULL)
-	while(fgets(line_in, 99, infile) != NULL)
-	    fputs(line_in, file);
-    else
-	fprintf(file, "\npostscript_prolog_is_missing\n\n");
+    {
+	FILE *infile = PaOpen("magicps", "r", ".pro", ".", SysLibPath, NULL);
+	if (infile != NULL)
+	{
+	    while(fgets(line_in, 99, infile) != NULL)
+		fputs(line_in, file);
+	    fclose(infile);
+	}
+	else
+	    fprintf(file, "\npostscript_prolog_is_missing\n\n");
+    }
 
     /* Insert the font definitions here. */
 

plot/plotVers.c
@@ -1353,7 +1353,7 @@ PlotVersatec(scx, layers, xMask, user_scale)
 		    {
 		        TxError("Warning:  No color versatec styles are defined"
 				" in the technology file!\nPlotting aborted.\n");
-			return;
+			goto error_close_only;
 		    }
 		    break;
 		default:
@@ -1372,7 +1372,7 @@ PlotVersatec(scx, layers, xMask, user_scale)
 		TxError("Warning:  No monochrome versatec styles are"
 			    " defined in the technology file!\nPlotting"
 			    " aborted.\n");
-		return;
+		goto error_close_only;
 	    }
 
 	    for ( ; curStyle != NULL; curStyle = curStyle->vs_next)
@@ -1475,10 +1475,14 @@ PlotVersatec(scx, layers, xMask, user_scale)
     }
     return;
 
-    error:
+error:
     TxError("\nVersatec plot aborted.\n");
     fclose(file);
     unlink(fileName);
+    return;
+
+error_close_only:
+    fclose(file);
 }
 
 #endif /* VERSATEC */

plow/PlowTest.c
@@ -143,7 +143,6 @@ PlowTest(w, cmd)
     Plane *plane;
     Edge edge;
     Tile *tp;
-    FILE *f;
 
     if (!ToolGetEditBox(&editArea) || !ToolGetBox(&rootBoxDef, &rootBox))
 	return;
@@ -244,19 +243,21 @@ PlowTest(w, cmd)
 	    plowYankDef = saveDef;
 	    break;
 	case PC_TECHSHOW:
-	    f = stdout;
 	    if (cmd->tx_argc >= 3)
 	    {
-		f = fopen(cmd->tx_argv[2], "w");
+		FILE *f = fopen(cmd->tx_argv[2], "w");
 		if (f == NULL)
 		{
 		    perror(cmd->tx_argv[2]);
 		    break;
 		}
+		plowTechShow(f);
+		fclose(f);
 	    }
-	    plowTechShow(f);
-	    if (f != stdout)
-		(void) fclose(f);
+	    else
+	    {
+		plowTechShow(stdout);
+	    }
 	    break;
 	case PC_WIDTH:
 	    if (cmd->tx_argc < 3)

commit 1457698506e36847b971a2a5e8d4e4130761e5ae

plotPNM.c: Fix crash due to uninited rtl_args.outfile
I introduced this issue in recent commit e88dcba1c

plot/plotPNM.c
@@ -587,6 +587,7 @@ PlotPNM(fileName, scx, layers, xMask, width)
 
 #ifdef VERSATEC
     struct plotRTLdata rtl_args;
+    rtl_args.outfile = NULL;
     char command[200], tempFile[200];
 #endif
 