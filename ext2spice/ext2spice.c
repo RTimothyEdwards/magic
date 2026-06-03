799fd5d8f258df8ba407d1531705b2e54d9e032a
Torleif Skår <16509259+tskaar@users.noreply.github.com>
2026-06-03 14:38:06 +0200

    fix(ext2spice): use ';' instead of '$' for NGSpice end-of-line comments
    '$' is only used by NGSpice, for broader compatibility use ';'.

--- ext2spice/ext2spice.c
+++ ext2spice/ext2spice.c
@@ -2817,7 +2817,7 @@ spcdevVisit(
 	case DEV_FET:
 	    if (source == drain)
 	    {
-		if (esFormat == NGSPICE) fprintf(esSpiceF, "$ ");
+		if (esFormat == NGSPICE) fprintf(esSpiceF, "; ");
 		fprintf(esSpiceF, "** SOURCE/DRAIN TIED\n");
 	    }
 	    break;
@@ -2825,7 +2825,7 @@ spcdevVisit(
 	default:
 	    if (gate == source)
 	    {
-		if (esFormat == NGSPICE) fprintf(esSpiceF, "$ ");
+		if (esFormat == NGSPICE) fprintf(esSpiceF, "; ");
 		fprintf(esSpiceF, "** SHORTED DEVICE\n");
 	    }
 	    break;
@@ -4111,7 +4111,7 @@ spcnodeVisit(
 	static char ntmp[MAX_STR_SIZE];
 
 	EFHNSprintf(ntmp, hierName);
-	if (esFormat == NGSPICE) fprintf(esSpiceF, "$ ");
+	if (esFormat == NGSPICE) fprintf(esSpiceF, "; ");
 	fprintf(esSpiceF, "** %s == %s\n", ntmp, nsn);
     }
     cap = cap  / 1000;
@@ -4121,14 +4121,14 @@ spcnodeVisit(
 	esSIvalue(esSpiceF, 1.0E-15 * cap);
 	if (!isConnected)
 	{
-	    if (esFormat == NGSPICE) fprintf(esSpiceF, " $");
+	    if (esFormat == NGSPICE) fprintf(esSpiceF, " ;");
 	    fprintf(esSpiceF, " **FLOATING");
 	}
 	fprintf(esSpiceF, "\n");
     }
     if (node->efnode_attrs && !esNoAttrs)
     {
-	if (esFormat == NGSPICE) fprintf(esSpiceF, " $ ");
+	if (esFormat == NGSPICE) fprintf(esSpiceF, " ; ");
 	fprintf(esSpiceF, "**nodeattr %s :",nsn );
 	for (fmt = " %s", ap = node->efnode_attrs; ap; ap = ap->efa_next)
 	{
